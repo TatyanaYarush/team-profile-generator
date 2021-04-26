b85cfa4721037376df6c6748a3315dd0f250420e
Tanya Y <brulek7@gmail.com>
2021-04-26 14:05:29 -0400

    created new files in scr and dist folder

--- index.js
+++ index.js
@@ -9,3 +9,58 @@ const fs = require("fs");
 const Logger = require("./logger");
 const log = new Logger();
 
+const generatePage = require();
+const writeFile = require()
+
+let manager = [];
+let engineer = [];
+let intern = [];
+let employeeArr = {manager, engineer, intern};
+
+function Prompt() {
+    
+  return inquirer
+      .prompt([
+      {
+          type: 'list',
+          name: 'role',
+          message:"What is the employee's role in AWTY company?",
+          choices: ['Manager', 'Engineer', 'Intern']
+          // log.red(choices); ///add color???
+           // if (choices) {
+        
+      },
+      {
+        type:'text',
+        name: 'employee',
+        message: "What is the employee's name?"
+    },
+    {
+      type:'text',
+      name: 'id',
+      message: "What is the employee's ID number?"
+  },
+  {
+      type: 'text',
+      name: 'email',
+      message: "What is the employee's email?"
+  }])
+  .then(({employee, id, email, role}) => {
+    if (role === "Manager") {
+        return inquirer
+            .prompt([{
+                type:'text',
+                name: 'office',
+                message:"What is the Manager's office number?"
+            },
+
+
+
+
+Prompt()
+    .then(teamData => {
+        return generatePage(employeeArr)
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML)
+    })
